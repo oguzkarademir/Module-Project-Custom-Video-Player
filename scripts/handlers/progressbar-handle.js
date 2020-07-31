bdcee37d2fd4d54ac444b732ab5449eea9cd10e5
Sayed Kazimi <61209285+Sayed94h@users.noreply.github.com>
2020-07-31 11:10:20 +0200

    Update progressbar-handle.js
    I did also the range because I misunderstood the range bar and progress bar

--- scripts/handlers/progressbar-handle.js
+++ scripts/handlers/progressbar-handle.js
@@ -1,13 +1,28 @@
 /* Get Our Elements (with querySelector)*/
+const progressBar = player.querySelector('.progress__filled');
+const ranges = player.querySelectorAll('.player__slider');
 
 
 
-/* when clicking on the progress bar,
-*  reaching that percentage of the video
+/* 
 *  (.currentTime - .duration - timeupdate event)
 */
-function handleProgressBar() { }
+function handleProgressBar() { 
 
+    const videoPercent = (video.currentTime / video.duration) * 100;
+   
+    progressBar.style.flexBasis = `${videoPercent}%`;
+
+}
+
+
+function handleRangeUpdate() {
+    video[this.name] = this.value;
+}
+
+
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
 
 
 
